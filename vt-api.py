98684bdf65297a478668cd3149bcb002ed123298
Nicolas Tosin - Soow <nicolas.tosin@soow.com.br>
2025-10-07 14:52:21 -0300

    Adicionado feature de HASH e argparse

--- vt-api.py
+++ vt-api.py
@@ -1,9 +1,64 @@
-import requests, time, csv
+import requests, time, csv, argparse, sys
 
 API_KEY = 'a688af4d3094202a9caf7ac4bada1eb066ea318981487ceead3526e3ab939717'
 HEADERS = {
     'x-apikey': API_KEY
 }
+
+def args():
+    parser = argparse.ArgumentParser(description="VTAPI")
+    parser.add_argument('-lip', '--IPLIST', type=str, help='Lista de IPs para consultar')
+    parser.add_argument('-ip', '--IP', type=str, help='IP para consultar')
+    parser.add_argument('-lhash', '--HASHLIST', type=str, help='Lista de HASHES para consultar')
+    parser.add_argument('-hash', '--HASH', type=str, default=None, help='Hash para consultar')
+    parser.add_argument('-o', '--OUTPUT', type=str, default=None, help='Output file')
+    return parser.parse_args()
+
+def verificar_hash(hash):
+    url = f"https://www.virustotal.com/api/v3/files/{hash}"
+    response = requests.get(url, headers=HEADERS)
+    
+    if response.status_code == 200:
+        response_json = response.json()
+        data = response_json["data"]
+        attributes = data["attributes"]
+        sha256 = attributes["sha256"]
+        name = attributes["meaningful_name"]
+        stats = attributes["last_analysis_stats"]
+        malicious = stats["malicious"]
+        sandbox_veredicts = attributes.get("sandbox_verdicts", {})
+        return sha256, malicious, name, sandbox_veredicts
+    else:
+        return None, None, None, None
+    
+def sandbox_veredict(hash):
+    resultado = verificar_hash(hash)
+    _, _, _, sandbox_veredict = resultado
+
+    if sandbox_veredict is None:
+        return None, None, None, None, None
+
+    print("--- Resultados das Sandboxes ---")
+    for sandbox_name, data in sandbox_veredict.items():
+        category = data.get("category", 'N/A')
+        sandbox_name = data.get("sandbox_name", [])
+        classification = ", ".join(data.get("malware_classification", []))
+        malware_names = ", ".join(data.get("malware_names", []))
+        confidence = data.get("confidence", "N/A") 
+        print(f"    > {sandbox_name} ({category.upper()})")
+        print(f"       - Classificacao: {classification}")
+        print(f"       - Nomes do Malware: {malware_names}")
+
+        if confidence != "N/A":
+            print(f"       - Confianca: {confidence}%")
+    print("-------------------------------")
+    return None, None, None, None, None
+
+def consulta_hash(hash):
+    sha256, malicious, name, _ = verificar_hash(hash)
+    print(f"Hash: \33[31m{sha256}\033[0m | Mal Score: \33[31m{malicious}\033[0m | File Name: \33[31m{name}\033[0m")
+    sandbox_veredict(hash)
+
 
 def verificar_ip(ip):
     url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
@@ -22,11 +77,14 @@ def verificar_ip(ip):
     else:
         return None, None, None, None
 
-def main():
-    with open('ips-origem.txt', 'r') as f:
+def consulta_lista_ip(lista_ips, arquivo_saida):
+    with open(lista_ips, 'r') as f:
         ips = [line.strip() for line in f if line.strip()]
 
-    with open('resultado.csv', 'w', newline='') as csvfile:
+    if not arquivo_saida.lower().endswith('.csv'):
+        arquivo_saida += '.csv'
+
+    with open(arquivo_saida, 'w', newline='') as csvfile:
         fieldnames = ['IP', 'Maliciosos', 'Suspeitos', 'Owner', 'Country']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -55,6 +113,28 @@ def main():
                     'Country': 'Erro'
                 })
             time.sleep(15)
+        print(f"Resultados salvos em {arquivo_saida}")
+
+
+def consulta_ip(ip):
+    maliciosos, suspeitos, owner, country = verificar_ip(ip)
+    if maliciosos is not None:
+        if maliciosos >= 2:
+            print(f"\33[31mIP: {ip}\033[0m | \33[31mMaliciosos: {maliciosos}\033[0m | Suspeitos: {suspeitos} | Owner: {owner} | Country: {country}")
+        else:
+            print(f"IP: {ip} | Maliciosos: {maliciosos} | Suspeitos: {suspeitos} | Owner: {owner} | Country: {country}")
+    
 
 if __name__ == "__main__":
-    main()
+    args = args()
+
+    if args.IP:
+        consulta_ip(args.IP)
+    if args.HASH:
+        consulta_hash(args.HASH)
+
+    if args.IPLIST:
+        if not args.OUTPUT:
+            print("Coloque um nome para o arquivo de saida!")
+            sys.exit(1)
+        consulta_lista_ip(args.IPLIST, args.OUTPUT)   
